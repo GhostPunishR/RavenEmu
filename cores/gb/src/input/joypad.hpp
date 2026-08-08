2413ef4ba64ce5bc3f176353f8d64d4033f66554
GhostPunishR <68479390+GhostPunishR@users.noreply.github.com>
2026-08-09 01:15:42 +0200

    feat(gb): réveiller STOP sur transition du joypad

--- cores/gb/src/input/joypad.hpp
+++ cores/gb/src/input/joypad.hpp
@@ -25,6 +25,7 @@ public:
         auto& state = action ? action_state_ : direction_state_;
         const bool was_pressed = (state & bit) != 0;
         state = pressed ? state | bit : state & ~bit;
+        if (pressed && !was_pressed) stop_wake_pending_ = true;
         const bool selected = (action && (select_ & 0x20) == 0) || (!action && (select_ & 0x10) == 0);
         if (pressed && !was_pressed && selected) interrupts_.request(Interrupt::joypad);
     }
@@ -36,14 +37,22 @@ public:
         return 0xc0 | select_ | (lines & 0x0f);
     }
     void write(int value) noexcept { select_ = value & 0x30; }
+
+    [[nodiscard]] bool take_stop_wake() noexcept {
+        const bool pending = stop_wake_pending_;
+        stop_wake_pending_ = false;
+        return pending;
+    }
+
     void save(BinaryWriter& out) const { out.i32(select_); }
-    void load(BinaryReader& in) { select_ = in.i32() & 0x30; }
+    void load(BinaryReader& in) { select_ = in.i32() & 0x30; stop_wake_pending_ = false; }
 
 private:
     InterruptController& interrupts_;
     int select_{0x30};
     int action_state_{};
     int direction_state_{};
+    bool stop_wake_pending_{};
 };
 
 } // namespace ravenemu::cgb
